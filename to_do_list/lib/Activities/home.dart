b8ce1d27f510b95a2e5d20bed7b29a7ae20b40c4
L3XxXa <alexkkk@inbox.ru>
2023-02-10 13:43:12 +0700

    List view

--- to_do_list/lib/Activities/home.dart
+++ to_do_list/lib/Activities/home.dart
@@ -1,10 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ToDoListApp extends StatelessWidget{
+class ToDoListApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ToDoListAppState();
+}
+
+class ToDoListAppState extends State<ToDoListApp> {
+  List todo = [];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int idx) {
+            return Dismissible(key: Key(todo[idx]), child: Card());
+          },
+          itemCount: todo.length),
+    );
   }
-
 }
