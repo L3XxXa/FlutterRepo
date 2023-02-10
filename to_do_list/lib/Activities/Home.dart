81f48441c996007d13c3b4ad9970c2e73d59feb8
L3XxXa <alexkkk@inbox.ru>
2023-02-10 11:36:17 +0700

    project init

---
+++ to_do_list/lib/Activities/Home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ToDoListApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ to_do_list/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/Activities/Home.dart';
+
+void main() {
+  runApp(ToDoListApp());
+}
