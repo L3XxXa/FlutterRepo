853097e9cb5036828afa436e1b740d4d2c47c907
L3XxXa <alexkkk@inbox.ru>
2023-02-11 10:40:45 +0700

    Completed app

--- to_do_list/lib/Activities/home.dart
+++ to_do_list/lib/Activities/home.dart
@@ -8,14 +8,74 @@ class ToDoListApp extends StatefulWidget {
 class ToDoListAppState extends State<ToDoListApp> {
   List todo = [];
 
+  String inputToDo = "";
+
+  @override
+  void initState() {
+    super.initState();
+    todo.addAll(["Buy something", "Wash hands", "Kill Bill"]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: ListView.builder(
           itemBuilder: (BuildContext context, int idx) {
-            return Dismissible(key: Key(todo[idx]), child: Card());
+            return Dismissible(
+                key: Key(todo[idx]),
+                child: Card(
+                    child: ListTile(
+                      title: Text(todo[idx]),
+                      trailing: IconButton(icon: const Icon(Icons.delete), onPressed: (){
+                        _deleteItem(idx);
+                      }),
+                    )
+                ),
+                onDismissed: (direction){
+                    _deleteItem(idx);
+                  }
+            );
           },
           itemCount: todo.length),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(context: context, builder: (BuildContext context){
+            return AlertDialog(
+              title: const Text("Add element"),
+              content: TextField(
+                onChanged: (String value){
+                  addSomething(value);
+                },
+              ),
+              actions: [
+                ElevatedButton(onPressed: (){
+                  addToTodo();
+                  Navigator.of(context).pop();
+                }, child: const Text("Add"))
+              ],
+            );
+          });
+        },
+        child: const Icon(Icons.add),),
     );
+  }
+
+  void addSomething(String value){
+    setState(() {
+      inputToDo = value;
+    });
+  }
+
+  void addToTodo(){
+    setState(() {
+      todo.add(inputToDo);
+    });
+  }
+
+  void _deleteItem(int idx) {
+    setState(() {
+      todo.removeAt(idx);
+    });
   }
 }
--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list/Activities/home.dart';
 
 void main() {
-  runApp(ToDoListApp());
+  runApp(MaterialApp(home: ToDoListApp()));
 }
